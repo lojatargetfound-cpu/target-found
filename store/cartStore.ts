fae98fbb432f432cdff7f7c4782f1877c7458e22
lojatargetfound-cpu <lojatargetfound@gmail.com>
2026-05-30 11:46:34 -0300

    fix clearCart

--- store/cartStore.ts
+++ store/cartStore.ts
@@ -1,18 +1,12 @@
 import { create } from 'zustand'
-
 import { persist } from 'zustand/middleware'
 
 interface Product {
   id: number
-
   name: string
-
   brand: string
-
   image: string
-
   price: number
-
   quantity: number
 }
 
@@ -32,110 +26,107 @@ interface CartStore {
   increaseQuantity: (id: number) => void
 
   decreaseQuantity: (id: number) => void
+
+  clearCart: () => void
 }
 
-export const useCartStore =
-  create<CartStore>()(
-    persist(
-      (set) => ({
-        cart: [],
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set) => ({
+      cart: [],
 
-        isOpen: false,
+      isOpen: false,
 
-        openCart: () =>
-          set({
-            isOpen: true,
-          }),
+      openCart: () =>
+        set({
+          isOpen: true,
+        }),
 
-        closeCart: () =>
-          set({
-            isOpen: false,
-          }),
+      closeCart: () =>
+        set({
+          isOpen: false,
+        }),
 
-        addToCart: (product) =>
-          set((state) => {
-            const existingProduct =
-              state.cart.find(
+      addToCart: (product) =>
+        set((state) => {
+          const existingProduct =
+            state.cart.find(
+              (item) =>
+                item.id === product.id
+            )
+
+          if (existingProduct) {
+            return {
+              cart: state.cart.map(
                 (item) =>
                   item.id === product.id
-              )
-
-            if (existingProduct) {
-              return {
-                cart: state.cart.map(
-                  (item) =>
-                    item.id === product.id
-                      ? {
-                          ...item,
-
-                          quantity:
-                            item.quantity + 1,
-                        }
-                      : item
-                ),
-              }
-            }
-
-            return {
-              cart: [
-                ...state.cart,
-
-                {
-                  ...product,
-
-                  quantity: 1,
-                },
-              ],
-            }
-          }),
-
-        removeFromCart: (id) =>
-          set((state) => ({
-            cart: state.cart.filter(
-              (item) => item.id !== id
-            ),
-          })),
-
-          clearCart: () =>
-  set({
-    cart: [],
-  }),
-        increaseQuantity: (id) =>
-          set((state) => ({
-            cart: state.cart.map(
-              (item) =>
-                item.id === id
-                  ? {
-                      ...item,
-
-                      quantity:
-                        item.quantity + 1,
-                    }
-                  : item
-            ),
-          })),
-
-        decreaseQuantity: (id) =>
-          set((state) => ({
-            cart: state.cart
-              .map((item) =>
-                item.id === id
-                  ? {
-                      ...item,
-
-                      quantity:
-                        item.quantity - 1,
-                    }
-                  : item
-              )
-              .filter(
-                (item) => item.quantity > 0
+                    ? {
+                        ...item,
+                        quantity:
+                          item.quantity + 1,
+                      }
+                    : item
               ),
-          })),
-      }),
+            }
+          }
 
-      {
-        name: 'target-found-cart',
-      }
-    )
+          return {
+            cart: [
+              ...state.cart,
+              {
+                ...product,
+                quantity: 1,
+              },
+            ],
+          }
+        }),
+
+      removeFromCart: (id) =>
+        set((state) => ({
+          cart: state.cart.filter(
+            (item) => item.id !== id
+          ),
+        })),
+
+      clearCart: () =>
+        set({
+          cart: [],
+        }),
+
+      increaseQuantity: (id) =>
+        set((state) => ({
+          cart: state.cart.map(
+            (item) =>
+              item.id === id
+                ? {
+                    ...item,
+                    quantity:
+                      item.quantity + 1,
+                  }
+                : item
+          ),
+        })),
+
+      decreaseQuantity: (id) =>
+        set((state) => ({
+          cart: state.cart
+            .map((item) =>
+              item.id === id
+                ? {
+                    ...item,
+                    quantity:
+                      item.quantity - 1,
+                  }
+                : item
+            )
+            .filter(
+              (item) => item.quantity > 0
+            ),
+        })),
+    }),
+
+    {
+      name: 'target-found-cart',
+    }
   )
+)
